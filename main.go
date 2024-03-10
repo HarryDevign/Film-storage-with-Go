c40e58063bcfa4d2e7ea602aca6764867c670a73
hoangle <hoangle@hiptechvn.com>
2024-03-10 14:19:59 +0700

    Display Server Data

--- main.go
+++ main.go
@@ -2,12 +2,31 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+type Film struct {
+	Title string
+	Director string
+}
+
 func main( ) { 
 	fmt.Println("Hello World")
+
+	h1 := func (w http.ResponseWriter, r *http.Request)  {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		films := map[string][]Film {
+			"Films": {
+				{Title: "The Godfather", Director:  "Francis Ford Coppola"},
+				{Title: "2001: A Space Odyssey", Director: "Stanley Kubrick"},
+				{Title: "The Thing", Director:  "Curtis Hanson"},
+			},
+		}
+		tmpl.Execute(w,  films)
+	}
+	http.HandleFunc("/", h1)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
